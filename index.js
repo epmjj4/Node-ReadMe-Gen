67e42bc8a2417202eafda909ff35fca68e82455d
Nes <epmjj4@hotmail.com>
2021-02-16 12:05:43 -0500

    created variables for inquirer, fs, and path

--- index.js
+++ index.js
@@ -2,18 +2,35 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
-
+// link to generate markdown.js in uitls folder
 const generateMarkDown = require("./utils/generateMarkDown");
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project'
+
+    },
+    {
+
+
+    }
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(answers => {
+        console.log('testing')
+    })
+}
 
 // Function call to initialize app
 init();
