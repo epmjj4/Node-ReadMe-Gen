15c53a7a4f60b9ff5de0f875680052a1a6224c9a
Nes <epmjj4@hotmail.com>
2021-02-25 09:26:04 -0500

    updated questions

--- index.js
+++ index.js
@@ -2,25 +2,52 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
-// link to generate markdown.js in uitls folder
+// link to generate markdown.js in utilities folder
 const generateMarkDown = require("./utils/generateMarkDown");
 
-
+function UserQuestion(question, dataName){
+    this.message = question;
+    this.type = 'input';
+    this.name = dataName;
+}
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project'
+function askFirstQuestion(){
+    inquirer.prompt([
 
-    },
-    {
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title'
+            
+        },
+        {
+            type: 'input',
+            message: 'What is the description of your project?',
+            name: 'description'
+    
+        },
+        {
+            type: 'input',
+            message: 'What is the table of contents for your project',
+            name: 'Table of Contents'
+    
+        },
+        {
+            type: 'input',
+            message: 'What is the table of contents for your project',
+            name: 'TOC'
+    
+        }
 
 
-    }
+    ]).then(response => {
 
-];
+        const {title,description, TOC, } = responses
 
+    }) 
+
+
+}
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
